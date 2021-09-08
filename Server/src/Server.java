2705ddfdff5ad4559261215b6aed48532a4f048f
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2021-09-08 13:26:06 -0400

    innit

---
+++ Server/src/Server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author nicol
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.print("Server Hello Word! :-)\n");
+	}
+
+
+}
