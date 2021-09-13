6d2a2be0c5991a7f64c8d40b637c01f2fff13ab9
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2021-09-13 19:12:00 -0400

    server from note de cours

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,6 +1,13 @@
-
+/**
+ * 
+ * @author nicol
+ *
+ */
 public class Client {
-
+/**
+ * 
+ * @param args
+ */
 	public static void main(String[] args) {
 		System.out.print("Client Hello Word! :-)\n");
 	}
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,19 +1,44 @@
-/**
- * 
- */
-
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.http.WebSocket.Listener;
 /**
  * @author nicol
  *
  */
 public class Server {
 
-	/**
-	 * @param args
-	 */
+	private static ServerSocket listener;
+	
 
-	public static void main(String[] args) {
-		System.out.print("Server Hello Word! :-)\n");
+	
+	public static void main(String[] args) throws Exception {
+		
+		int nClient =0;
+		
+		String serverAddress ="127.0.0.1";
+		int serverPort = 5012;	
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIP = InetAddress.getByName(serverAddress);
+		
+		listener.bind(new InetSocketAddress(serverIP,serverPort));
+	
+		System.out.format("The server is running on %s:%d%n",serverAddress, serverPort);
+		
+		try {
+			while(true) {
+				new ClientHandler(listener.accept(),nClient++).start();		
+				}
+			
+		} finally {
+			listener.close();
+		}
+	
+	
 	}
 
 
