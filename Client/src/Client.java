864c8f17cc3f4657508d71747b826d7cf9cbc490
raphaelfilion22 <raphaelfilion22@gmail.com>
2021-09-13 19:17:34 -0400

    Client notes de cours

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,8 +1,32 @@
+import java.io.DataInputStream;
+import java.net.Socket;
 
-public class Client {
-
-	public static void main(String[] args) {
-		System.out.print("Client Hello Word! :-)\n");
+public class Client 
+{
+	private static Socket socket;
+	
+	/*
+	 * Application client
+	 */
+	public static void main(String[] args) throws Exception 
+	{
+		// Adresse et port du serveur
+		String serverAdress = "127.0.0.1";
+		int port = 5012;
+		
+		// Création d'une nouvelle connexion avec le serveur
+		socket = new Socket(serverAdress, port);
+		
+		System.out.format("The server is running on %s:%d%n", serverAdress, port);
+		
+		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		// Attente de la réception d'un message envoyé par le serveur sur le canal
+		String helloMessageFromServer = in.readUTF();
+		System.out.println(helloMessageFromServer);
+		
+		// Fermeture de la connexion avec le serveur
+		socket.close();
 	}
-
 }
