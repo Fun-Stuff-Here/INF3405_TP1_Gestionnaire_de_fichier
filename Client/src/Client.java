d6301de87ab5ae08ebb51fd810ae306edc4c7186
Nicolas Depelteau <71102008+Depdx@users.noreply.github.com>
2021-09-13 20:37:54 -0400

    port and ip validation

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,10 +1,59 @@
 
 import java.io.DataInputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client 
 {
 	private static Socket socket;
+	
+	
+private static String getIPAddress() {
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+			System.out.println("Enter a valid IP address:");
+			String iP = scanner.nextLine();
+			
+			String[] ipByte = iP.split("\\.");
+			if(ipByte.length !=4) throw new IllegalArgumentException("Ip address should be of lenght 4");
+			for (String string : ipByte) {
+				int octet = Integer.parseInt(string);
+				if(octet <0 || octet >255) throw new IllegalArgumentException("Ip address should have four values between 0 and 255");
+			}
+			
+			System.out.println("IP address: " + iP);
+			return iP;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		}
+
+
+		
+	}	
+	
+private static int getPort() {
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+			System.out.println("Enter a valid Port number: ");
+			String portString = scanner.nextLine();
+			
+		
+			int port = Integer.parseInt(portString);
+			if(port <5002 || port >5049) throw new IllegalArgumentException("Port must be between 5002 and 5049");
+
+			System.out.println("Port number: " + port);
+			return port;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		}
+}
+	
 	
 	/*
 	 * Application client
@@ -12,8 +61,8 @@ public class Client
 	public static void main(String[] args) throws Exception 
 	{
 		// Adresse et port du serveur
-		String serverAdress = "127.0.0.1";
-		int port = 5012;
+		String serverAdress = getIPAddress();
+		int port = getPort();
 		
 		// Création d'une nouvelle connexion avec le serveur
 		socket = new Socket(serverAdress, port);
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -5,6 +5,8 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.http.WebSocket.Listener;
+import java.text.ParseException;
+import java.util.Scanner;
 /**
  * @author nicol
  *
@@ -16,12 +18,69 @@ public class Server {
 	 */
 	private static ServerSocket listener;
 
+	
+	
+	private static String getIPAddress() {
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+			System.out.println("Enter a valid IP address:");
+			String iP = scanner.nextLine();
+			
+			String[] ipByte = iP.split("\\.");
+			if(ipByte.length !=4) throw new IllegalArgumentException("Ip address should be of lenght 4");
+			for (String string : ipByte) {
+				int octet = Integer.parseInt(string);
+				if(octet <0 || octet >255) throw new IllegalArgumentException("Ip address should have four values between 0 and 255");
+			}
+			
+			System.out.println("IP address: " + iP);
+			return iP;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		}
+
+
+		
+	}	
+	
+private static int getPort() {
+		
+		Scanner scanner = new Scanner(System.in);
+		while(true) {
+			try {
+			System.out.println("Enter a valid Port number: ");
+			String portString = scanner.nextLine();
+			
+		
+			int port = Integer.parseInt(portString);
+			if(port <5002 || port >5049) throw new IllegalArgumentException("Port must be between 5002 and 5049");
+
+			System.out.println("Port number: " + port);
+			return port;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		}
+
+
+		
+	}	
+	
+	
 	public static void main(String[] args) throws Exception {
 
 		int nClient =0;
 
-		String serverAddress ="127.0.0.1";
-		int serverPort = 5012;	
+		
+		
+		String serverAddress = getIPAddress();
+		int serverPort = getPort();	
+		
+		
+		
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
@@ -41,6 +100,9 @@ public class Server {
 
 
 	}
+	
+
+	
 }
 
 
